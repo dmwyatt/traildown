8248ed2bd58400f8cca43b284e373d038a9470fe
Dustin Wyatt <thermopyle@contriving.net>
2011-01-11 13:58:04 -0600

    threaded downloading

--- Downloader.java
+++ Downloader.java
@@ -3,108 +3,73 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.io.File;;
+import java.io.File;
 
-public class Downloader {
+public class Downloader implements Runnable {
 	
-	private BlockingQueue<DownloadMe> downloads;
-
-	public Downloader() {
-		downloads = new LinkedBlockingQueue<DownloadMe>(); 
+	private DownloadMe downMe;
+	
+	public Downloader(DownloadMe download) {
+		downMe = download; 
 	}
 	
-	public void addDownload(String url, String dlpath, String id) {
-		DownloadMe download = new DownloadMe(url, dlpath, id);
-		downloads.add(download);
-		System.out.printf("Added %s to queue\n", download.ident);
-	}
-	
-	public void start() {
-		// Up to three concurrent downloads
-//		Runnable downloadJob = new ThreadedDownloader(); 
-//		Thread tDownloader = new Thread(downloadJob);
-//		tDownloader.start();
-		ThreadedDownloader tDowner = new ThreadedDownloader();
-		tDowner.run();
-	}
-	
-	public class ThreadedDownloader implements Runnable {
-		public void run() {
-			ExecutorService pool = Executors.newFixedThreadPool( 2 );
-			while (true) {
-				ThreadedDownload downloadThread = new ThreadedDownload();			
-				pool.execute( downloadThread );
-			}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			DownloadFile();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("DownloadFile() crashed");
+			e.printStackTrace();
 		}
+	}
+	
+	private URLConnection SetConxProps(URL url, URLConnection uc){
+		if (url.getHost().toLowerCase().contains("apple.com")) {
+			uc.setRequestProperty("User-Agent", "QuickTime");
+		}
+		if (url.getHost().toLowerCase().contains("yahoo.com")) {
+			uc.setRequestProperty("User-Agent", "Firefox");
+			uc.setRequestProperty("Referer", "http://movies.yahoo.com/");
+		}
+		return uc;
+	}
+	
+	private void DownloadFile() throws Exception {
+		//Set up connection
+		String url = downMe.downloadUrl;
+		String saveLocation = downMe.downloadTo;
 		
-		public class ThreadedDownload implements Runnable {
-			public void run() {
-				// Get oldest url added to downloadUrls and download it
-				try {
-					// This blocks until there is a download
-					DownloadMe downloading = downloads.take();
-					System.out.printf("Fetching %s\n", downloading.ident);
-					DownloadFile(downloading.downloadUrl, downloading.downloadTo);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}						
-			}
-			
-			private URLConnection SetConxProps(URL url, URLConnection uc){
-				if (url.getHost().toLowerCase().contains("apple.com")) {
-					uc.setRequestProperty("User-Agent", "QuickTime");
-				}
-				if (url.getHost().toLowerCase().contains("yahoo.com")) {
-					uc.setRequestProperty("User-Agent", "Firefox");
-					uc.setRequestProperty("Referer", "http://movies.yahoo.com/");
-				}
-				return uc;
-			}
-			
-			private void DownloadFile(String url, String saveLocation) throws Exception {
-				//Set up connection
-				
-				
-				URL u = new URL(url);
-				URLConnection uc = u.openConnection();
-				uc = SetConxProps(u, uc);
-				
-		        BufferedInputStream in = null;
-		        FileOutputStream fout = null;
-		        try
-		        {
-		        	System.out.println("Preparing conx...");
-		        	InputStream ucStream = uc.getInputStream();
-		            in = new BufferedInputStream(ucStream);
-		            fout = new FileOutputStream(saveLocation);
+		URL u = new URL(url);
+		URLConnection uc = u.openConnection();
+		uc = SetConxProps(u, uc);
+		
+        BufferedInputStream in = null;
+        FileOutputStream fout = null;
+        try
+        {
+        	System.out.println("Preparing conx...");
+        	InputStream ucStream = uc.getInputStream();
+            in = new BufferedInputStream(ucStream);
+            fout = new FileOutputStream(saveLocation);
 
-		            byte data[] = new byte[1024];
-		            int count;
-		            System.out.printf("....fetching %d bytes from %s\n", uc.getContentLength(), url);
-		            while ((count = in.read(data, 0, 1024)) != -1)
-		            {
-		                    fout.write(data, 0, count);
-		            }
-		        }
-		        finally
-		        {
-		                if (in != null)
-		                        in.close();
-		                if (fout != null)
-		                        fout.close();
-		                File fn = new File(saveLocation);
-		        	    System.out.println(fn.getCanonicalPath());
-		        }
-
-			}
-		}
+            byte data[] = new byte[1024];
+            int count;
+            System.out.printf("....fetching %d bytes from %s\n", uc.getContentLength(), url);
+            while ((count = in.read(data, 0, 1024)) != -1)
+            {
+                    fout.write(data, 0, count);
+            }
+        }
+        finally
+        {
+                if (in != null)
+                        in.close();
+                if (fout != null)
+                        fout.close();
+                File fn = new File(saveLocation);
+        	    System.out.println(fn.getCanonicalPath());
+        }
 	}
 }
--- Td.java
+++ Td.java
@@ -1,6 +1,9 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class Td {
-	public static void main(String[] args){
+	public static void main(String[] args){		
 		String d1url = "http://playlist.yahoo.com/makeplaylist.dll?sdm=web&pt=rd&sid=114763383";
 		String d1fn = "kings_speech.mov";
 		String d1id = "The King's Speech";
@@ -23,31 +26,20 @@ public class Td {
 		
 		String d6url = "http://playlist.yahoo.com/makeplaylist.dll?sdm=web&pt=rd&sid=115387498";
 		String d6fn = "true_grit.mov";
-		String d6id = "True Grit";
+		String d6id = "True Grit";	
 		
-//		String d1url = "http://www.contriving.net/ftpd/zscreen/2010/screen-0023-12-29.png";
-//		String d1fn = "kings_speech.png";
-//		String d1id = "The King's Speech";
+		ExecutorService downloadPool = Executors.newFixedThreadPool(2); //Two simultaneous downloads
+//		Downloader downloadJob4 = new Downloader(new DownloadMe(d4url, d4fn, d4id));
+//		downloadPool.execute(downloadJob4);
 //		
-//		String d2url = "http://www.contriving.net/ftpd/zscreen/2010/screen-0022-12-27.png";
-//		String d2fn = "green_hornet.png";
-//		String d2id = "The Green Hornet";
+//		Downloader downloadJob1 = new Downloader(new DownloadMe(d1url, d1fn, d1id));
+//		downloadPool.execute(downloadJob1);
 //		
-//		String d3url = "http://www.contriving.net/ftpd/zscreen/2010/screen-0021-12-23.png";		
-//		String d3fn = "star_wars.png";
-//		String d3id = "Star Wars";
+//		Downloader downloadJob2 = new Downloader(new DownloadMe(d2url, d2fn, d2id));
+//		downloadPool.execute(downloadJob2);
 //		
-//		String d4url = "http://www.contriving.net/ftpd/zscreen/2010/screen-0020-12-22.png";
-//		String d4fn = "the_mechanic.png";
-//		String d4id = "The Mechanic";
-		
-		Downloader dler = new Downloader();
-		
-		
-		dler.addDownload(d4url, d4fn, d4id);
-		dler.addDownload(d1url, d1fn, d1id);
-		dler.addDownload(d2url, d2fn, d2id);		
-		dler.addDownload(d3url, d3fn, d3id);
-		dler.start();
+//		Downloader downloadJob3 = new Downloader(new DownloadMe(d3url, d3fn, d3id));
+//		downloadPool.execute(downloadJob3);
+
 	}
 }
